2fbcacc27881cbe36d633d4b362ca18d39ed4182
Aïmane Zelloufi <150799549+giovaimane@users.noreply.github.com>
2023-12-07 10:20:05 +0100

    Update Transmission_Reception.ino
    - Rajout d'un USART3 et USART0 afin de communiquer entre 2 appareils à l'aide d'une Arduino Mega

--- Transmission_Reception.ino
+++ Transmission_Reception.ino
@@ -38,26 +38,41 @@
 #define MYUBRR FOSC / 16 / BAUD - 1
 
 /*! \brief 
- * - La déclaration unsigned char data crée une variable data capable de stocker des entiers non signés de 8 bits. 
- * - Elle est utilisée pour stocker des données reçues via la communication série USART. 
+ * - La déclaration unsigned char data0 crée une variable data capable de stocker des entiers non signés de 8 bits. 
+ * - Elle est utilisée pour stocker des données reçues via la communication série USART0. 
  * - La taille de la variable est limitée à des valeurs entre 0 et 255.
  */
-unsigned char data;
+unsigned char data0;
+
 
 /*! \brief 
- * - La déclaration unsigned char flag = 0; crée une variable nommée flag de type entier non signé de 8 bits, initialement mise à 0. 
+ * - La déclaration unsigned char data3 crée une variable data capable de stocker des entiers non signés de 8 bits. 
+ * - Elle est utilisée pour stocker des données reçues via la communication série USART3. 
+ * - La taille de la variable est limitée à des valeurs entre 0 et 255.
+ */
+unsigned char data3;
+
+/*! \brief 
+ * - La déclaration unsigned char flag0 = 0; crée une variable nommée flag de type entier non signé de 8 bits, initialement mise à 0. 
  * - Cette variable est souvent utilisée comme un indicateur (drapeau) dans les programmes pour signaler un état particulier. 
- * - Dans ce contexte, elle est employée pour indiquer si des données ont été reçues via la communication série USART, avec la valeur 1 pour indiquer la réception de données et 0 sinon.
+ * - Dans ce contexte, elle est employée pour indiquer si des données ont été reçues via la communication série USART0, avec la valeur 1 pour indiquer la réception de données et 0 sinon.
  */
-unsigned char flag =0;
+unsigned char flag0 =0;
 
 /*! \brief 
- * - La fonction USART_Init initialise la communication série USART sur un microcontrôleur AVR. 
- * - Elle configure le registre de baud rate avec la valeur fournie, active le récepteur et le transmetteur USART, active l'interruption de réception pour permettre l'utilisation d'interruptions lors de la réception de données, et fixe le format du cadre de communication avec 8 bits de données et 1 bit d'arrêt. 
- * - Cette configuration standard assure une communication asynchrone avec des caractéristiques spécifiques. 
- * - L'utilisation de la fonction USART_Init simplifie la configuration de la communication série dans les programmes principaux en appelant simplement cette fonction.
+ * - La déclaration unsigned char flag3 = 0; crée une variable nommée flag de type entier non signé de 8 bits, initialement mise à 0. 
+ * - Cette variable est souvent utilisée comme un indicateur (drapeau) dans les programmes pour signaler un état particulier. 
+ * - Dans ce contexte, elle est employée pour indiquer si des données ont été reçues via la communication série USART3, avec la valeur 1 pour indiquer la réception de données et 0 sinon.
  */
-void USART_Init(unsigned int ubrr) {
+unsigned char flag3 =0;
+
+/*! \brief 
+ * - La fonction USART0_Init initialise la communication série USART0 sur un microcontrôleur AVR. 
+ * - Elle configure le registre de baud rate avec la valeur fournie, active le récepteur et le transmetteur USART0, active l'interruption de réception pour permettre l'utilisation d'interruptions lors de la réception de données, et fixe le format du cadre de communication avec 8 bits de données et 1 bit d'arrêt. 
+ * - Cette configuration standard assure une communication asynchrone avec des caractéristiques spécifiques. 
+ * - L'utilisation de la fonction USART0_Init simplifie la configuration de la communication série dans les programmes principaux en appelant simplement cette fonction.
+ */
+void USART0_Init(unsigned int ubrr) {
   /*Set baud rate */
   UBRR0H = (unsigned char)(ubrr >> 8);
   UBRR0L = (unsigned char)ubrr;
@@ -68,23 +83,52 @@ void USART_Init(unsigned int ubrr) {
 }
 
 /*! \brief 
- * - La fonction USART_Transmit transmet un octet de données sur la communication série USART d'un microcontrôleur AVR.
+ * - La fonction USART3_Init initialise la communication série USART sur un microcontrôleur AVR. 
+ * - Elle configure le registre de baud rate avec la valeur fournie, active le récepteur et le transmetteur USART3, active l'interruption de réception pour permettre l'utilisation d'interruptions lors de la réception de données, et fixe le format du cadre de communication avec 8 bits de données et 1 bit d'arrêt. 
+ * - Cette configuration standard assure une communication asynchrone avec des caractéristiques spécifiques. 
+ * - L'utilisation de la fonction USART3_Init simplifie la configuration de la communication série dans les programmes principaux en appelant simplement cette fonction.
+ */
+void USART3_Init(unsigned int ubrr) {
+  /*Set baud rate */
+  UBRR3H = (unsigned char)(ubrr >> 8);
+  UBRR3L = (unsigned char)ubrr;
+  /*Enable receiver and transmitter */
+  UCSR3B = (1 << TXEN3) | (1 << RXEN3) | (1 << RXCIE3);
+  /* Set frame format: 8data, 1stop bit */
+  UCSR3C =  (3 << UCSZ30);
+}
+
+/*! \brief 
+ * - La fonction USART0_Transmit transmet un octet de données sur la communication série USART0 d'un microcontrôleur AVR.
  * - La boucle while assure l'attente du tampon de transmission vide.
  * - Le placement de la donnée dans le tampon de données (UDR0) déclenche le début de la transmission.
  * - Cette fonction est essentielle pour garantir une transmission synchrone, évitant la perte de données en attendant que le tampon soit disponible.
  */
-void USART_Transmit(unsigned char data) {
+void USART0_Transmit(unsigned char data0) {
   /* Wait for empty transmit buffer */
   while (!(UCSR0A & (1 << UDRE0)))    ;
   /* Put data into buffer, sends the data */
-  UDR0 = data;
+  UDR0 = data0;
 }
 
 /*! \brief 
- * - La fonction USART_Receive attend que le tampon de réception soit prêt en vérifiant le bit d'indicateur d'attente de tampon plein (RXC0), puis retourne les données reçues du registre de données de réception (UDR0). 
- * - Cette fonction facilite la récupération synchrone des données reçues sur la communication série USART sans risque de lire des données non prêtes.
+ * - La fonction USART3_Transmit transmet un octet de données sur la communication série USART3 d'un microcontrôleur AVR.
+ * - La boucle while assure l'attente du tampon de transmission vide.
+ * - Le placement de la donnée dans le tampon de données (UDR3) déclenche le début de la transmission.
+ * - Cette fonction est essentielle pour garantir une transmission synchrone, évitant la perte de données en attendant que le tampon soit disponible.
  */
-unsigned char USART_Receive() {
+void USART3_Transmit(unsigned char data3) {
+  /* Wait for empty transmit buffer */
+  while (!(UCSR3A & (1 << UDRE3)))    ;
+  /* Put data into buffer, sends the data */
+  UDR3 = data3;
+}
+
+/*! \brief 
+ * - La fonction USART0_Receive attend que le tampon de réception soit prêt en vérifiant le bit d'indicateur d'attente de tampon plein (RXC0), puis retourne les données reçues du registre de données de réception (UDR0). 
+ * - Cette fonction facilite la récupération synchrone des données reçues sur la communication série USART0 sans risque de lire des données non prêtes.
+ */
+unsigned char USART0_Receive() {
   // Attente que le tampon de réception soit prêt
   while (!(UCSR0A & (1 << RXC0)));
 
@@ -94,35 +138,71 @@ unsigned char USART_Receive() {
 
 
 /*! \brief 
- * - Cette interruption de réception USART (ISR(USART_RX_vect)) est une fonction à exécuter lorsque des données sont reçues sur la communication série. 
- * - Elle récupère la donnée reçue (data=UDR0;) dans la variable data et active le drapeau (flag=1;). 
+ * - La fonction USART3_Receive attend que le tampon de réception soit prêt en vérifiant le bit d'indicateur d'attente de tampon plein (RXC3), puis retourne les données reçues du registre de données de réception (UDR3). 
+ * - Cette fonction facilite la récupération synchrone des données reçues sur la communication série USAR3 sans risque de lire des données non prêtes.
+ */
+unsigned char USART3_Receive() {
+  // Attente que le tampon de réception soit prêt
+  while (!(UCSR3A & (1 << RXC3)));
+
+  // Retourner les données reçues du registre de données et incrémenter le code ASCII
+  return UDR3;
+}
+
+
+/*! \brief 
+ * - Cette interruption de réception USART (ISR(USART0_RX_vect)) est une fonction à exécuter lorsque des données sont reçues sur la communication série. 
+ * - Elle récupère la donnée reçue (data0=UDR0;) dans la variable data et active le drapeau (flag0=1;). 
  * - Cette approche est couramment utilisée pour indiquer dans le programme principal qu'une nouvelle donnée est disponible et peut être traitée. 
  * - La variable flag pourrait être utilisée comme un indicateur pour déclencher des actions dans le code principal en réponse à la réception de données.
  */
-ISR(USART_RX_vect) {
-  // Fonction à exécuter lors de l'interruption de réception USART
-  data=UDR0;
-  flag=1;
+ISR(USART0_RX_vect) {
+  // Fonction à exécuter lors de l'interruption de réception USART0
+  //data0=UDR0;
+  flag0=1;
   
   
 }
 
 /*! \brief 
- * - Le programme principal initialise les interruptions globales et la communication série USART avec un certain taux de bauds. 
- * - Ensuite, dans la boucle infinie, il vérifie périodiquement si des données ont été reçues (indiqué par le drapeau flag). 
+ * - Cette interruption de réception USART (ISR(USART3_RX_vect)) est une fonction à exécuter lorsque des données sont reçues sur la communication série. 
+ * - Elle récupère la donnée reçue (data3=UDR3;) dans la variable data et active le drapeau (flag3=1;). 
+ * - Cette approche est couramment utilisée pour indiquer dans le programme principal qu'une nouvelle donnée est disponible et peut être traitée. 
+ * - La variable flag pourrait être utilisée comme un indicateur pour déclencher des actions dans le code principal en réponse à la réception de données.
+ */
+ISR(USART3_RX_vect) {
+  // Fonction à exécuter lors de l'interruption de réception USART3
+  //data3=UDR3;
+  flag3=1;
+  
+  
+}
+
+/*! \brief 
+ * - Le programme principal initialise les interruptions globales et la communication série USART0 et USART3 avec un certain taux de bauds. 
+ * - Ensuite, dans la boucle infinie, il vérifie périodiquement si des données ont été reçues (indiqué par le drapeau flag0 et flag3). 
  * - Si c'est le cas, il transmet ces données incrémentées de 1 via la communication série. 
  * - Le délai de 1 milliseconde (_delay_ms(1)) assure une temporisation courte entre les itérations de la boucle principale. 
  * - Cette structure de programme permet une réactivité aux interruptions, adaptée à la communication asynchrone.
  */
 int main(void) {
+ 
+  USART0_Init(MYUBRR);
+  USART3_Init(MYUBRR);
+  DDRB=0b10000000;
   sei();  // Activer les interruptions globales
-  USART_Init(MYUBRR);
-
   while (1) {
 
-    if(flag){
-      USART_Transmit(data+1);
-      flag=0;
+    if(flag3){
+      USART0_Transmit(UDR3);
+      flag3=0;
+      PORTB = 0b10000000;
+    }
+
+        if(flag0){
+      USART3_Transmit(UDR0);
+      flag0=0;
+      PORTB = 0b10000000;
     }
 
    _delay_ms(1);
@@ -130,8 +210,3 @@ int main(void) {
     
   }
 }
-
-
-
-
-
